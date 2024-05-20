282512363eb7cdf823c47c0ccedaccc6ef410cd8
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-05-20 13:47:55 +0200

    a

--- API/routes/prueba.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-//Mio
-/* GET users listing. */
-router.get('/', function(req, res) {
-    res.send('este es mi json');
-});
-
-module.exports = router;
