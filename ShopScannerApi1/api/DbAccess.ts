175d592a1d2850d2fc6315ab319a9f7f9383f7a8
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-05-22 18:00:38 +0200

    a

---
+++ ShopScannerApi1/api/DbAccess.ts
@@ -0,0 +1,14 @@
+//GetPagina(int n, String match , 30)   30 es el tamaño de la pagina
+
+//GetUsuario(String nombre, String passw)   El usuario es un dao como el de c# (para mantener la integridad de la app)
+
+//UpdateLista(Lista l , int idUsuario)  Update de esa lista de ese usuario
+
+//PostLista(Lista l , int idUsuario)    inserta la lista a ese usuario
+
+//SendMail (Lista l , int idUsuario) manda el mail al usuario de esa lista (Lo ideal seria un pdf, si no con un mail de texto plano vale)
+
+//Futuras mejoras: la seguridad, que sea un pdf la lista, optimizacion del uso de la memoria 
+//(ya sea cambiando el formato de las imagenes en la fase de escrapeo, o ya sea tambien guardandolas como bytea (no lo hago aun por el uso de espacio y ancho de banda))
+//revisar la estetica, escalar la aplicacion escrapeando mas supermercados
+
--- ShopScannerApi1/api/index.ts
+++ ShopScannerApi1/api/index.ts
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
-
+//TODO: falta seguir el tutorial de posgres, en una carpeta a parte hacer una clase de acc a datos
+//      para el jueves llevar al menos un json de productos
+//
+//      hacer un script que me lea el csv de la db y la convierta en un solo insert
+//      
+//
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/resp1", (req, res) => res.send("resp1"));
 app.get("/resp2", (req, res) => res.send("resp1"));
