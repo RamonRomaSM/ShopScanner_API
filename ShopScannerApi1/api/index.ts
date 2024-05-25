2661c058706027a3e9c60aab7a60f07b7a8fe06e
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-05-25 13:53:01 +0200

    Update index.ts

--- ShopScannerApi1/api/index.ts
+++ ShopScannerApi1/api/index.ts
@@ -19,7 +19,7 @@ POSTGRES_PASSWORD="LgdV3Hc7UFKI"
 POSTGRES_DATABASE="verceldb"
 */ 
 db.connect();
-const datos = await db`SELECT * FROM tfgapi-postgres.productos LIMIT 5`;
+const datos = await db`SELECT * FROM productos LIMIT 5`;
 
 app.get("/", (req, res) => res.send(res.json({datos})));
 
