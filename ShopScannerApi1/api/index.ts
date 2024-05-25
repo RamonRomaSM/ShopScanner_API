57eaccae7fe6760519735e3981df402aa12883c4
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-05-25 12:48:37 +0200

    a

--- ShopScannerApi1/api/index.ts
+++ ShopScannerApi1/api/index.ts
@@ -1,3 +1,4 @@
+import { sql } from '@vercel/postgres';
 const express = require("express");
 const app = express();
 //TODO: falta seguir el tutorial de posgres, en una carpeta a parte hacer una clase de acc a datos
@@ -6,8 +7,21 @@ const app = express();
 //      hacer un script que me lea el csv de la db y la convierta en un solo insert
 //      
 //
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
+/*
+POSTGRES_URL="postgres://default:LgdV3Hc7UFKI@ep-quiet-feather-a2l8vl1h-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require"
+POSTGRES_PRISMA_URL="postgres://default:LgdV3Hc7UFKI@ep-quiet-feather-a2l8vl1h-pooler.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
+POSTGRES_URL_NO_SSL="postgres://default:LgdV3Hc7UFKI@ep-quiet-feather-a2l8vl1h-pooler.eu-central-1.aws.neon.tech:5432/verceldb"
+POSTGRES_URL_NON_POOLING="postgres://default:LgdV3Hc7UFKI@ep-quiet-feather-a2l8vl1h.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require"
+POSTGRES_USER="default"
+POSTGRES_HOST="ep-quiet-feather-a2l8vl1h-pooler.eu-central-1.aws.neon.tech"
+POSTGRES_PASSWORD="LgdV3Hc7UFKI"
+POSTGRES_DATABASE="verceldb"
+*/ 
+app.get("/", (req, res) => res.send( sql`SELECT * FROM productos WHERE precio < 10 LIMIT 5;`));
+
 app.get("/resp1", (req, res) => res.send("resp1"));
+
 app.get("/resp2", (req, res) => res.send("resp1"));
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
