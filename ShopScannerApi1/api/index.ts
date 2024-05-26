b9815975fd3b70699b0d1caa8d535a19490e86e0
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-05-26 20:08:26 +0200

    Update index.ts

--- ShopScannerApi1/api/index.ts
+++ ShopScannerApi1/api/index.ts
@@ -27,7 +27,7 @@ app.get("/", async function(req, res) {
 
 app.get("/aa", async function(  req: Express.TypedRequestQuery<{ page: Number}>,res: Express.Response) {
     const client = await db.connect();
-    const a = await client.sql`SELECT * FROM productos LIMIT 1,1;`;
+    const a = await client.sql`SELECT * FROM productos LIMIT ${req.page};`;
     res.status(200).json({a});
 });
 
