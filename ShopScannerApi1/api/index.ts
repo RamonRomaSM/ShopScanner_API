4e8c39a74f05ff276ea044112fb97fca5f22a930
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-05-26 19:49:42 +0200

    Update index.ts

--- ShopScannerApi1/api/index.ts
+++ ShopScannerApi1/api/index.ts
@@ -1,4 +1,6 @@
 import { db } from '@vercel/postgres';
+import Express from 'express';
+import http from 'http';
 const express = require("express");
 const app = express();
 //TODO: falta seguir el tutorial de posgres, en una carpeta a parte hacer una clase de acc a datos
@@ -6,6 +8,9 @@ const app = express();
 //
 //      hacer un script que me lea el csv de la db y la convierta en un solo insert
 //      
+//      SELECT * FROM tfg.productos LIMIT 0,5
+//      el 0 se refiere a la fila por la que empieza 
+//      el 5 el numero de filas que recoje a partir del dado
 //
 
 /*
@@ -19,12 +24,12 @@ app.get("/", async function(req, res) {
     const a = await client.sql`SELECT * FROM productos LIMIT 5;`;
     res.status(200).json({a});
 });
-app.get("/aa", async function(req, res) {
+
+app.get("/aa", async function(  req: Express.TypedRequestQuery<{ pag: string}>,res: Express.Response) {
+
     const client = await db.connect();
-    const a = await client.sql`SELECT * FROM productos LIMIT 1;`;
-    res.status(200).json({
-        req : (req as Request).body
-    });
+    const a = await client.sql`SELECT * FROM productos LIMIT ${req.pag},1;`;
+    res.status(200).json({ USUARIO: req.username });
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
