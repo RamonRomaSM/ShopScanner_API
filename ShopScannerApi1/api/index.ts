d78443032efe483cc8a4d76e995c03113812b5dc
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-05-29 19:58:04 +0200

    Update index.ts

--- ShopScannerApi1/api/index.ts
+++ ShopScannerApi1/api/index.ts
@@ -46,7 +46,7 @@ app.get("/getPagina", async function( req,res ) {
     res.status(200).json({a});
 });
 
-app.get("/register/:nombre/aaa/:passw",async function (req,res) {
+app.get("/register/nombre/:nombre/aaa/:passw",async function (req,res) {
     res.send(req.params);
     //comprobar si existe(pedirlo y ver si retorna vacio?) e insertarlo
     //usar posgres
