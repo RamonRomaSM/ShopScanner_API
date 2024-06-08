1662c656dd439d31a629c7587bfff1d05a211107
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-06-08 10:32:40 +0200

    Update index.ts

--- ShopScannerApi1/api/index.ts
+++ ShopScannerApi1/api/index.ts
@@ -5,28 +5,14 @@ import http from 'http';
 const express = require("express");
 const app = express();
 
-/*
-    aparentemente se puede meter un json en el body de una peticion post
-    asi que meter ahi a lista que se quiere guardar
 
-    createLista (nombre, idUser, cadena de productos...)    
-        te la añado y te mando de vuelta tus listas 
-
-    updateLista (cadena de productos...)
-        me pasas la lista nueva y la sobreescribo
-        te mando de vuelta tus listas
-
-    deleteLista (nombre idUser)
-*/
 
 app.get("/", async function(req, res) {
     res.send("Respuesta por defecto");
 });
 
 app.get("/getPagina/:num/:hint", async function( req,res ) {
-/*
-    % foo % ya deberia funcionar, falta agregar %foo%
-*/ 
+ 
     let num = req.params.num * 30;
     let hint;
     if(req.params.hint == "Escribe lo que quieras buscar"){hint ='%';}
@@ -48,17 +34,17 @@ app.get("/register/nombre/:nombre/passw/:passw",async function (req,res) {
 
     if(existe == false){
         client.sql`INSERT INTO usuarios (nombre, passw) VALUES (${req.params.nombre}, ${req.params.passw});`
-        res.status(200).send(true);
+        res.status(200).send("resp:"+true);
     }
     else{
-        res.status(200).send(false);
+        res.status(200).send("resp:"+false);
     }
 });
 
 app.get("/login/nombre/:nombre/passw/:passw",async function(req,res) {
     const client = await db.connect();
     const exists = await client.sql`SELECT * FROM usuarios WHERE nombre = ${req.params.nombre} AND passw = ${req.params.passw};`;
-    //exists tiene el boolean que me dice si ya existe, sacarlo del json y hacer un if, por ultimo un req.send(true o false o algo asi)
+   //tengo que darte tu usuario + listas
     res.status(200).json({exists});
 });
 
